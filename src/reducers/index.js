0efe00e118f60da493975384ef6321d6980fc5fb
Anant8002749 <ak8002749@gmail.com>
2020-12-31 12:29:36 +0530

    Added combineReducer package of redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
+import  { combineReducers } from 'redux';
 import {ADD_MOVIES, ADD_FAVOURITE, REMOVE_FAVOURITE, SET_SHOW_FAVOURITE } from '../actions';
 
 const initialMoviesState = {
@@ -53,9 +54,13 @@ const initialRootState={
     movies:initialMoviesState,
     search:initialSearchState
 };
-export default function rootReducer(state=initialRootState, action){
-    return{
-        movies: movies(state.movies,action),
-        search:search(state.search,action)
-    }
-}
+// export default function rootReducer(state=initialRootState, action){
+//     return{
+//         movies: movies(state.movies,action),
+//         search:search(state.search,action)
+//     }
+// }
+export default combineReducers({
+    movies,
+    search
+});
