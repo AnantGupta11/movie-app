7d3607aae2470cc4101133c57030dee7d2ff9e69
Anant8002749 <ak8002749@gmail.com>
2021-01-03 11:32:43 +0530

    modifying middleware

--- src/index.js
+++ src/index.js
@@ -7,13 +7,18 @@ import rootReducer from './reducers';
 
 //function logger (obj,next,action)
 //logger(obj)(next)(action)
-const logger = function({dispatch, getState}){
-    return function(next){
-        return function(action){
-            console.log('ACTION_TYPE= ',action.type);
-            next(action);
-        }
-    }
+// const logger = function({dispatch, getState}){
+//     return function(next){
+//         return function(action){
+//             console.log('ACTION_TYPE= ',action.type);
+//             next(action);
+//         }
+//     }
+// }
+
+const logger = ({dispatch, getState})=>(next)=>(action)=>{
+        console.log('ACTION_TYPE= ',action.type);
+        next(action);
 }
 
 
