b910db6f2e88fbe16f30398642c084aabcbd573e
Anant8002749 <ak8002749@gmail.com>
2021-01-04 12:37:22 +0530

    Using context api1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import './index.css';
@@ -39,14 +39,33 @@ const logger = ({dispatch, getState})=>(next)=>(action)=>{
 const store = createStore(rootReducer, applyMiddleware(logger,thunk));
 
 // console.log('store',store);
-// console.log('STATE',store.getState());
+ console.log('STATE',store.getState());
+
+export const StoreContext = createContext();
+console.log('StoreContext',StoreContext);
+
+class Provider extends React.Component{
+    render(){
+        const { store }=this.props;
+        return<StoreContext.Provider value={store}>
+            {this.props.children}
+        </StoreContext.Provider>
+
+    }
+}
 
 // store.dispatch({
 //     type:'ADD_MOVIES',
 //     movies: [{name:'Superman'}]
 // });
 
-ReactDOM.render(<><App store={store} /></>,document.getElementById('root'));
+ReactDOM.render(
+<>
+<Provider store={store}>
+    <App />
+</Provider>
+</>,
+document.getElementById('root'));
 
 
 
